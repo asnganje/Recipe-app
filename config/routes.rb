b7c699052db9021844e40ae66aec14dcaa629dad
Josué Benavides <jdbs9514@gmail.com>
2023-02-07 12:22:26 -0500

    setup the app

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :foods
+  resources :recipe_foods
+  resources :inventory_foods
+  resources :recipes
+  resources :inventories
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
