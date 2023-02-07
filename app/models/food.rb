e9c13ce8ed04f939be1aae22d8a2d47be6dd20d5
asnganje <abd.nganje@gmail.com>
2023-02-07 20:59:27 +0300

    Add model relationships

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,6 @@
 class Food < ApplicationRecord
+    has_many :inventory_food
+    has_many :recipe_food
+    has_many :inventory, through: :inventory_food
+    has_many :recipe, through: :recipe_food
 end
