9aad9a5af7995ebbe08e5e3a30753a5512205ac6
Josué Benavides <jdbs9514@gmail.com>
2023-02-09 16:40:59 -0500

    added the recipe view and controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,11 +3,15 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.includes(:user).where(user_id: current_user)
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @food = Food.new
+    @recipe = Recipe.find(params[:id])
+    @recipe_food = RecipeFood.new
+  end
 
   # GET /recipes/new
   def new
@@ -20,6 +24,7 @@ class RecipesController < ApplicationController
   # POST /recipes or /recipes.json
   def create
     @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
 
     respond_to do |format|
       if @recipe.save
@@ -64,6 +69,6 @@ class RecipesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def recipe_params
-    params.fetch(:recipe, {})
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
   end
 end
