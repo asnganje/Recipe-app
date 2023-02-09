e29768292f6801f919fc2e793a819aa6339c9937
asnganje <abd.nganje@gmail.com>
2023-02-09 19:52:54 +0300

    Add food list feature

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -64,6 +64,6 @@ class FoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def food_params
-    params.fetch(:food, {})
+    params.require(:food).permit(:name, :measurement_unit, :price, :user_id)
   end
 end
