8c16c31f8b4af4cbb6879e4389b12fd4c3358499
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-10 08:09:15 +0300

    Fix linters

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,8 +3,8 @@ class RecipeFoodsController < ApplicationController
 
   # GET /recipe_foods or /recipe_foods.json
   def index
-  #  @recipe_food = RecipeFood.new
-      @recipe_food = current_user.recipes
+    #  @recipe_food = RecipeFood.new
+    @recipe_food = current_user.recipes
   end
 
   # GET /recipe_foods/1 or /recipe_foods/1.json
